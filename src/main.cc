589ee6e9fc5b232c42dc4d80161fd8193c4cd268
Łukasz Twarduś <ltwardus@gmail.com>
2016-01-15 18:31:02 +0100

    [] Algorithm now uses futures

--- src/main.cc
+++ src/main.cc
@@ -21,6 +21,44 @@ T clamp(const T&n, const T& lower, const T& upper) {
   return std::max(lower, std::min(n, upper));
 }
 
+void generate_end_evaluate_solution(
+    cv::Mat& tiles,
+    cv::Mat& image,
+    double& distance,
+    const cv::Mat& original_image_lab,
+    std::mt19937& rnd_engine,
+    std::uniform_int_distribution<int>& generate_random_tile_row,
+    std::uniform_int_distribution<int>& generate_random_tile_col,
+    std::normal_distribution<> generate_random_color_channel_value,
+    cv::Mat& image_lab) {
+  const int kPixelSize = std::ceil(image.cols / (tiles.cols * 1.0f));
+
+  /** Mutate candidate */
+  cv::Vec3b& random_tile =
+    tiles.at<cv::Vec3b>(generate_random_tile_row(rnd_engine), generate_random_tile_col(rnd_engine));
+  for (int i = 0; i < 3; ++i) {
+    random_tile.val[i] =
+      clamp(random_tile.val[i] + static_cast<int>(generate_random_color_channel_value(rnd_engine)), 0, 255);
+  }
+
+  for (int x = 0; x < tiles.cols; ++x) {
+    for (int y = 0; y < tiles.rows; ++y) {
+      const cv::Point kBeginPoint(x * kPixelSize, y * kPixelSize);
+      const cv::Point kEndPoint(kBeginPoint.x + kPixelSize, kBeginPoint.y + kPixelSize);
+      cv::rectangle(
+        image,
+        kBeginPoint,
+        kEndPoint,
+        cv::Scalar(tiles.at<cv::Vec3b>(y, x)),
+        CV_FILLED);
+    }
+  }
+
+  cv::cvtColor(image, image_lab, CV_BGR2Lab);
+
+  distance = cv::norm(original_image_lab, image_lab);
+}
+
 int main(int argc, char* argv[]) {
   if (4 != argc) {
     print_help();
@@ -45,17 +83,15 @@ int main(int argc, char* argv[]) {
 
   std::cout << "Press ESC to save output image and exit ..." << std::endl;
 
-  {
-    const std::string kOriginalImageWindowName = "Original image";
-    cv::namedWindow(kOriginalImageWindowName, CV_GUI_EXPANDED);
-    cv::imshow(kOriginalImageWindowName, original_image);
-  }
+  const std::string kOriginalImageWindowName = "Original image - Press ESC to save output image and exit ...";
+  cv::namedWindow(kOriginalImageWindowName, CV_GUI_EXPANDED);
+  cv::imshow(kOriginalImageWindowName, original_image);
 
   {
     cv::Mat generated_image = original_image.clone();
     generated_image = cv::Scalar();
 
-    const std::string kGeneratedImageWindowName = "Generated image";
+    const std::string kGeneratedImageWindowName = "Generated image - Press ESC to save output image and exit ...";
     cv::namedWindow(kGeneratedImageWindowName, CV_GUI_EXPANDED);
     cv::imshow(kGeneratedImageWindowName, generated_image);
 
@@ -72,100 +108,52 @@ int main(int argc, char* argv[]) {
 
     const unsigned kNumWorkers = std::max(static_cast<unsigned>(1), std::thread::hardware_concurrency());
 
-    std::vector<std::thread> workers(kNumWorkers);
-    std::vector<std::mutex> mutexes(kNumWorkers);
-    std::vector<std::condition_variable> condition_variables(kNumWorkers);
-    std::vector<std::atomic_bool> work_available_flags(kNumWorkers);
-    std::vector<std::atomic_bool> stop_working_flags(kNumWorkers);
+    std::vector<std::future<void>> results(kNumWorkers);
+    std::vector<std::mt19937> rnd_engines(kNumWorkers);
+    std::vector<std::uniform_int_distribution<int>>
+      generate_random_tile_rows(kNumWorkers, std::uniform_int_distribution<int>(0, kNumTilesY - 1));
+    std::vector<std::uniform_int_distribution<int>>
+      generate_random_tile_cols(kNumWorkers, std::uniform_int_distribution<int>(0, kNumTilesX - 1));
+    std::vector<std::normal_distribution<>>
+      generate_random_color_channel_values(kNumWorkers, std::normal_distribution<>(0, 32));
     std::vector<double> distances(kNumWorkers, -1);
     std::vector<cv::Mat> tiles(kNumWorkers);
     std::vector<cv::Mat> images(kNumWorkers);
+    std::vector<cv::Mat> images_lab(kNumWorkers);
     for (unsigned i = 0; i < kNumWorkers; ++i) {
+      rnd_engines[i] = std::mt19937(std::random_device{}());
       tiles[i] = best_tiles.clone();
       images[i] = generated_image.clone();
+      images_lab[i] = generated_image.clone();
     }
 
-    for (unsigned i = 0; i < kNumWorkers; ++i) {
-      workers[i] = std::thread([](
-          std::mutex& m,
-          std::condition_variable& cv,
-          std::atomic_bool& work_available_flag,
-          std::atomic_bool& stop_working_flag,
-          cv::Mat& tiles,
-          cv::Mat& image,
-          double& distance,
-          const cv::Mat& original_image_lab) {
-        std::unique_lock<std::mutex> lock(m);
-
-        std::mt19937 gen(std::random_device{}());
-        std::uniform_int_distribution<int> generate_random_tile_row(0, tiles.rows - 1);
-        std::uniform_int_distribution<int> generate_random_tile_col(0, tiles.cols - 1);
-        std::normal_distribution<> generate_random_color_channel_value(0, 32);
-        const int kPixelSize = std::ceil(image.cols / (tiles.cols * 1.0f));
-
-        while(!stop_working_flag) {
-          cv.wait(lock, [&work_available_flag, &stop_working_flag]() -> bool {
-            return work_available_flag || stop_working_flag;
-          });
-
-          if (stop_working_flag) {
-            return;
-          }
-
-          work_available_flag = false;
-
-          /** Mutate candidate */
-          cv::Vec3b& random_tile =
-            tiles.at<cv::Vec3b>(generate_random_tile_row(gen), generate_random_tile_col(gen));
-          for (int i = 0; i < 3; ++i) {
-            random_tile.val[i] =
-              clamp(random_tile.val[i] + static_cast<int>(generate_random_color_channel_value(gen)), 0, 255);
-          }
-
-          for (int x = 0; x < tiles.cols; ++x) {
-            for (int y = 0; y < tiles.rows; ++y) {
-              const cv::Point kBeginPoint(x * kPixelSize, y * kPixelSize);
-              const cv::Point kEndPoint(kBeginPoint.x + kPixelSize, kBeginPoint.y + kPixelSize);
-              cv::rectangle(
-                image,
-                kBeginPoint,
-                kEndPoint,
-                cv::Scalar(tiles.at<cv::Vec3b>(y, x)),
-                CV_FILLED);
-            }
-          }
-
-          cv::Mat image_lab;
-          cv::cvtColor(image, image_lab, CV_BGR2Lab);
-
-          distance = cv::norm(original_image_lab, image_lab);
-        }
-      },
-      std::ref(mutexes[i]),
-      std::ref(condition_variables[i]),
-      std::ref(work_available_flags[i]),
-      std::ref(stop_working_flags[i]),
-      std::ref(tiles[i]),
-      std::ref(images[i]),
-      std::ref(distances[i]),
-      std::cref(original_image_lab));
-    }
-
-    auto last_time_gui_update = std::chrono::system_clock::now();
+    std::chrono::system_clock::time_point last_time_gui_update;
+    const int kGuiUpdateIntervalMs = 1000;
 
     while(true) {
       for (unsigned i = 0; i < kNumWorkers; ++i) {
-        std::unique_lock<std::mutex> lock(mutexes[i]);
         tiles[i] = best_tiles.clone();
-        work_available_flags[i] = true;
-        condition_variables[i].notify_all();
+
+        results[i] =
+          std::async(
+            std::launch::async,
+            generate_end_evaluate_solution,
+            std::ref(tiles[i]),
+            std::ref(images[i]),
+            std::ref(distances[i]),
+            std::cref(original_image_lab),
+            std::ref(rnd_engines[i]),
+            std::ref(generate_random_tile_rows[i]),
+            std::ref(generate_random_tile_cols[i]),
+            std::ref(generate_random_color_channel_values[i]),
+            std::ref(images_lab[i]));
       }
 
       /** Wait for results and also get best candidate */
       double best_candidate_distance = -1;
       unsigned best_candidate_index = 0;
       for (unsigned i = 0; i < kNumWorkers; ++i) {
-        std::unique_lock<std::mutex> lock(mutexes[i]);
+        results[i].get();
         if (-1 == best_candidate_distance || distances[i] < best_candidate_distance) {
           best_candidate_distance = distances[i];
           best_candidate_index = i;
@@ -173,27 +161,21 @@ int main(int argc, char* argv[]) {
       }
 
       if (-1 == best_distance || best_candidate_distance < best_distance) {
-        std::unique_lock<std::mutex> lock(mutexes[best_candidate_index]);
         best_distance = best_candidate_distance;
         generated_image = images[best_candidate_index].clone();
         best_tiles = tiles[best_candidate_index].clone();
-        cv::imshow(kGeneratedImageWindowName, generated_image);
       }
 
       {
         auto now = std::chrono::system_clock::now();
-        if (std::chrono::duration_cast<std::chrono::milliseconds>(now - last_time_gui_update).count() >= 1000) {
+        const auto& gui_update_time_diff_ms =
+          std::chrono::duration_cast<std::chrono::milliseconds>(now - last_time_gui_update).count();
+        if (gui_update_time_diff_ms >= kGuiUpdateIntervalMs) {
+          cv::imshow(kOriginalImageWindowName, original_image);
+          cv::imshow(kGeneratedImageWindowName, generated_image);
           last_time_gui_update = now;
           /** Wait for ESC key */
           if (27 == static_cast<char>(cv::waitKey(1))) {
-            for (unsigned i = 0; i < kNumWorkers; ++i) {
-              stop_working_flags[i] = true;
-              condition_variables[i].notify_one();
-              if (workers[i].joinable()) {
-                workers[i].join();
-              }
-            }
-
             break;
           }
         }
